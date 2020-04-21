0c9028a3d8f890e0d2e9d81f4d4bb55ef636faf2
Lucien George <luciengeorge95@gmail.com>
2020-04-21 19:34:49 +0100

    typo

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer
-  attr_accessor :id, :name, :address # attr_accessor for :nam, and :address necessary to update the customer
+  attr_accessor :id, :name, :address # attr_accessor for :name, and :address necessary to update the customer
   def initialize(attr = {})
     @id = attr[:id]
     @name = attr[:name]
